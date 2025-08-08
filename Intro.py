d2eb1dfcb0b2f21baa4c31eabc98b38c94e3ea18
FranklineOduor <franklinoduor090@gmail.com>
2025-08-08 23:17:27 +0300

    Initial commit of Python intro and assignment files

---
+++ Intro.py
@@ -0,0 +1,28 @@
+PLP = "Academy"
+Stdent_Name = "Michael_Savio"
+Student_ID = "11223344"
+Student_Age = "20"
+Course_Name = "Python_Programming"
+#Output
+print(Stdent_Name, Student_ID, Student_Age)
+#Lists
+languages = {"Python", "Java", "C++"}
+print(languages)
+#Dictionary
+student_info = {
+    "Name": Stdent_Name,
+    "ID": Student_ID,
+    "Age": Student_Age,
+    "Course": Course_Name
+}
+print(student_info)
+#Tuple  
+student_tuple = (Stdent_Name, Student_ID, Student_Age, Course_Name)
+print(student_tuple)
+#Logical Operators
+a = 10
+b = 20
+c = 30
+print(a < b and b < c)  # True
+Site_Name = "PLP Academy"
+print(Site_Name.startswith("PLP"))
